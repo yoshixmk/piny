84d62301e0bf5454f2981537172ff73361d3a84f
yoshixmk <pirori67@gmail.com>
2021-01-03 13:41:05 +0900

    refactor

--- mod.ts
+++ mod.ts
@@ -1,57 +1,42 @@
 import { bgRed, green, red } from "https://deno.land/std/fmt/colors.ts";
-import { bgBlue, BufReader, flagParse, parse, sprintf } from "./deps.ts";
+import { bgBlue, flagParse, sprintf } from "./deps.ts";
+import { DictionaryFactory } from "./dictionary_factory.ts";
 import { pinyinToAlphabet } from "./pinyin_to_alphabet.ts";
 
-const [hsk3File, hsk4File, hsk5File, hsk6File] = await Promise.all([
-  Deno.open("./dict/HSK3-word-japanese.csv"),
-  Deno.open("./dict/HSK4-word-japanese.csv"),
-  Deno.open("./dict/HSK5-word-japanese.csv"),
-  Deno.open("./dict/HSK6-word-japanese.csv"),
-]);
+const dictionary = await DictionaryFactory.create(
+  "./dict/HSK3-word-japanese.csv",
+  "./dict/HSK4-word-japanese.csv",
+  "./dict/HSK5-word-japanese.csv",
+  "./dict/HSK6-word-japanese.csv",
+);
+const startIndex = await readStartIndex();
+dictionary.skip(startIndex);
+const args = flagParse(Deno.args);
+for (const word of dictionary.getWords()) {
+  console.log(`---------------
+    ${word.index}
+${word.mean}`);
+  const input = prompt(word.han);
 
-var startLine;
-try {
-  startLine = await Deno.readTextFile("./dict/dict.idx");
-} catch (e) {
-  startLine = "HSK3-1";
+  if (args.say) {
+    Deno.run({
+      cmd: ["say", "-v", "Ting-Ting", word.han],
+    });
+  }
+  if (pinyinToAlphabet(word.pin) === input) {
+    console.log(
+      green(sprintf(`%${word.han.length * 2}-s`, "◎")),
+      bgBlue(word.pin),
+    );
+    continue;
+  }
+  console.log(red(sprintf(`%${word.han.length * 2}-s`, "×")), bgRed(word.pin));
 }
 
-try {
-  const buf3 = BufReader.create(hsk3File);
-  const buf4 = BufReader.create(hsk4File);
-  const buf5 = BufReader.create(hsk5File);
-  const buf6 = BufReader.create(hsk6File);
-  const result = await Promise.all(
-    [parse(buf3), parse(buf4), parse(buf5), parse(buf6)],
-  );
-  // console.dir(result.flat());
-
-  for (const record of result.flat() as string[5][]) {
-    const [_, han, pin, mean, index] = record;
-    if (startLine && startLine !== index) {
-      continue;
-    }
-    startLine = undefined;
-    console.log(`---------------
-    ${index}
-${mean}`);
-    const input = prompt(han);
-
-    if (flagParse(Deno.args).say) {
-      Deno.run({
-        cmd: ["say", "-v", "Ting-Ting", han],
-      });
-    }
-    if (pinyinToAlphabet(pin) === input) {
-      console.log(green(sprintf(`%${han.length * 2}-s`, "◎")), bgBlue(pin));
-      continue;
-    }
-    console.log(red(sprintf(`%${han.length * 2}-s`, "×")), bgRed(pin));
-    // console.dir(record);
+async function readStartIndex() {
+  try {
+    return await Deno.readTextFile("./dict/dict.idx");
+  } catch (e) {
+    return "HSK3-1";
   }
-} finally {
-  hsk3File.close();
-  hsk4File.close();
-  hsk5File.close();
-  hsk6File.close();
 }
